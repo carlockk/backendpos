d4b46a561b740e7a09ca78e033469e6f9b428474
carlockk <carlos.virtualdesk@gmail.com>
2025-06-02 02:37:36 -0400

    🔧 Mejoras

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ app.use('/api/categorias', categoriaRoutes);
 app.use('/api/tickets', ticketRoutes);
 
 // Conexión a MongoDB
+console.log('🌍 MONGO_URI:', process.env.MONGO_URI);
 mongoose.connect(process.env.MONGO_URI, {
   //useNewUrlParser: true,
   //useUnifiedTopology: true
